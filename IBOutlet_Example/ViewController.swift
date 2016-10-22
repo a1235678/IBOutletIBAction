b534a26ffce039c137d702b5ac9de38a08a344f9
zeroplus <z1235678@gmail.com>
2016-10-22 17:45:39 +0800

    new

--- IBOutlet_Example/ViewController.swift
+++ IBOutlet_Example/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordsTextView: UITextView!
+    @IBOutlet weak var insertTextField: UITextField!
+    
+    @IBAction func enterButton(_ sender: AnyObject) {
+        if insertTextField.text != nil{
+            wordsTextView.text = wordsTextView.text + "\n" + insertTextField.text!
+            insertTextField.text = nil
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
